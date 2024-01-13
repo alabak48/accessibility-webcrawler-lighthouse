74b3da24eabea859d6c83ba515082f607be47be9
Anja <anja.labak12345@gmail.com>
2024-01-13 12:25:30 +0100

    promjena na desktop verziju

--- index.mjs
+++ index.mjs
@@ -9,7 +9,16 @@ async function runLighthouse(url, index) {
   const options = { 
     logLevel: 'error', 
     output: 'json',
-    port: chrome.port 
+     formFactor: 'desktop',
+    screenEmulation: {
+        mobile: false,
+        width: 1350,
+        height: 940,
+        deviceScaleFactor: 1,
+        disabled: false,
+    },
+    emulatedUserAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    port: chrome.port
   };
 
   try {
